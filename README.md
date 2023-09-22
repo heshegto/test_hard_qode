5048cd9c7458804873167ea79ba370e8870ee6af
heshegto <heshkin@gmail.com>
2023-09-22 20:45:07 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>HardQode test task</h1>
+
+- This my test task for job Python trainee in HardQode company
+- My name Heshegto Badneev
+- My <a href="https://hh.ru/resume/e85341bfff0c21a0a80039ed1f5938796c4f49">CV</a> in HH.ru 
+- My mail: <a href="mailto:heshkin@gmail.com"> heshkin@gmail.com <a>
---
+++ my_test_app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=128)
+    url_adress = models.URLField()
+    length = models.TimeField()
+
+
+class User(AbstractUser):
+    video_watch = models.ManyToManyField(to=Lesson, through="VideoWatch")
+
+
+class Product(models.Model):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    accesses = models.ManyToManyField(User)
+
+
+class VideoWatch(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    time_stop = models.FloatField(
+        default=0.0,
+        validators=[
+            MaxValueValidator(1.0),
+            MinValueValidator(0.0)
+        ]
+    )
+    is_watched = models.BooleanField(default=False)
