5529141194c8ba16b25b2f43814040f445e6d49e
heshegto <heshkin@gmail.com>
2023-09-25 00:33:58 +0300

    File main.html updated. Task 2.2 done. Created API that shows list of lessons of concrete product to which current user has access

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from api.views import UserListAPIView
+from api.views import UserListAPIView, ProductListAPIView
 
 app_name = 'api'
 urlpatterns = [
     path('user_list/', UserListAPIView.as_view(), name='user_list'),
+    path('user_list/<int:product_id>', ProductListAPIView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.generics import ListAPIView
-from my_test_app.models import User
-from api.serializers import UserSerializer
+from my_test_app.models import User, Accesses, Product
+from api.serializers import UserSerializer, AccessesSerializer, ProductSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
+# Задание 2.1
 class UserListAPIView(ListAPIView):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
@@ -11,3 +12,16 @@ class UserListAPIView(ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return User.objects.filter(pk=user.pk)
+
+
+# Задание 2.2
+class ProductListAPIView(ListAPIView):
+    serializer_class = ProductSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        if Accesses.objects.filter(user=self.request.user, accesses=self.kwargs.get('product_id')):
+            return Product.objects.filter(id=self.kwargs.get('product_id'))
+        else:
+            return None
+
